0f2d9010a79ccacdd6016eb7b5203b740ca310f3
jjchern <jiajia.chern@gmail.com>
2017-07-24 12:13:33 -0500

    Reprocess the raw county file
    * Use the xlsx dictionary file
    * Add variable labels

--- R/data.R
+++ R/data.R
@@ -1,11 +1,30 @@
 #' AHRF County Data Layout
+#'
+#' Contains the layout file for the AHRF County Data.
+#'
+#' @md
+#' @source [https://datawarehouse.hrsa.gov/topics/ahrf.aspx]()
 "ahrf_county_layout"
 
 #' AHRF County Data
+#'
+#' Contains county-level data from the Area Health Resources Files.
+#'
+#' @md
+#' @source [https://datawarehouse.hrsa.gov/topics/ahrf.aspx]()
 "ahrf_county"
 
 #' AHRF State Data Layout
+#'
+#' Contains the layout file for the AHRF State Data.
+#'
+#' @md
+#' @source [https://datawarehouse.hrsa.gov/topics/ahrf.aspx]()
 "ahrf_state_layout"
 
 #' AHRF State Data
+#'
+#' Contains state-level data from the Area Health Resources Files.
+#' @md
+#' @source [https://datawarehouse.hrsa.gov/topics/ahrf.aspx]()
 "ahrf_state"
--- data-raw/prep_county.R
+++ data-raw/prep_county.R
@@ -1,86 +1,69 @@
 
-library(purrr, warn.conflicts = FALSE)
-library(dplyr, warn.conflicts = FALSE)
-library(stringr)
-library(readr)
+library(tidyverse)
+library(readxl)
+library(labelled)
 
-url = "http://datawarehouse.hrsa.gov/DataDownload/ARF/AHRF_2014-2015.zip"
+# Download raw files ------------------------------------------------------
+
 url = "https://datawarehouse.hrsa.gov/DataDownload/AHRF/AHRF_2015-2016.ZIP"
-
 fil_zip = tempfile(fileext = ".zip")
 
-if(!file.exists("county/ahrf2015.asc")) {
+if(!file.exists("data-raw/county/ahrf2016.asc")) {
         download.file(url, fil_zip)
-        dir.create("county")
-        unzip(fil_zip, exdir = "county", junkpaths = TRUE)
+        dir.create("data-raw/county")
+        unzip(fil_zip, exdir = "data-raw/county", junkpaths = TRUE)
 }
-list.files("county")
+list.files("data-raw/county")
 
-raw_src = "county/ahrf2016.asc" # Raw data
-dic_src = "county/ahrf2015-16.sas" # SAS dictionary file
+raw_src = "data-raw/county/ahrf2016.asc" # Raw data
+dic_src = "data-raw/county/ahrf2015-16.sas" # SAS dictionary file
+doc_src = "data-raw/county/AHRF 2015-2016 Technical Documentation.xlsx"
 
-# Prepare layout file -----------------------------------------------------
+#' Previously the layout file is based on the SAS dictionary file
+#' Turned out the xlsx file is easier to process
+#' readxl::read_excel(doc_src) %>% View
 
-dic = readr::read_lines(dic_src)
+# Find out the line for the first field: F00001 ---------------------------
 
-# First, parse the SAS dictionary file to get variable name and width
-dic[1:20] # the SAS command `input` start in line 3
-layout = SAScii::parse.SAScii(dic_src, beginline = 3) %>% tbl_df
-layout
+read_excel(doc_src) %>%
+        pull(X__1) %>%
+        grepl("F00001", .) %>%
+        which() -> bgn_line
+bgn_line
 
-# Next, add variable labels
-# There are two sets of variables labels in the SAS dictionary file:
-# (1) after SAS command `label`, and (2) Within comments `/* */`
-# Most of the time these two sets of labels give the same information.
-# Here we keep both of them.
+# Prepare the layout file -------------------------------------------------
 
-# Get the first part of labels
-label_1_bgn_line = which(str_detect(dic, "label") == TRUE)
-label_1 = dic[label_1_bgn_line:length(dic)]
-label_1 = label_1[which(str_detect(label_1, "="))]
-
-l1 = data_frame(l = label_1) %>%
-        tidyr::separate(l, c("varname", "label_1"), sep = "\t=\t") %>%
-        mutate(varname = str_trim(varname) %>% str_to_upper(),
-               label_1 = str_trim(label_1))
-l1
-
-# Get the second part of labels
-label_2_bgn_line = which(str_detect(dic, "input") == TRUE)
-label_2 = dic[label_2_bgn_line:label_1_bgn_line]
-label_2 = label_2[which(str_detect(label_2, "/*"))]
-label_2 = label_2[3:6965]
-label_2 = str_trim(label_2)
-label_2
-l2 = data_frame(l = label_2) %>%
-        tidyr::separate(l, c("varname", "label_2"), sep = "\\/\\*") %>%
-        mutate(varname = str_extract(varname, "(f\\d*)") %>% str_to_upper()) %>%
-        mutate(label_2 = label_2 %>% str_sub(1, -3) %>% str_trim())
-l2
-
-# Combine layout with variable labels
-layout %>%
-        left_join(l1, by = "varname") %>%
-        left_join(l2, by = "varname") -> ahrf_county_layout
+read_excel(doc_src,
+           col_names = c("field", "col_col", "year_of_data", "var_label",
+                         "characteristics", "source", "date_on"),
+           skip = bgn_line) %>%
+        ## All filed starts with F and then some number
+        filter(grepl("^F[0-9]", field)) %>%
+        separate(col_col, c("col_start", "col_end")) %>%
+        mutate_at(c("col_start", "col_end"), as.integer) -> ahrf_county_layout
 ahrf_county_layout
 
-# Load the data
+# Prepare the county AHRF file --------------------------------------------
+
 read_fwf(file = raw_src,
-         col_positions = fwf_widths(widths = ahrf_county_layout$width,
-                                    col_names = ahrf_county_layout$varname)) -> ahrf_county
+         col_positions = fwf_positions(start = ahrf_county_layout$col_start,
+                                       end = ahrf_county_layout$col_end,
+                                       col_names = ahrf_county_layout$field)) -> ahrf_county
 ahrf_county
 
-# Variable labels ---------------------------------------------------------
+# Add variable labels -----------------------------------------------------
 
-# ahrf_county[] <- map2(ahrf_county, layout$label_1, function(x, label) {
-#         if (is.na(label)) {
-#                 x
-#         } else {
-#                 structure(x, label = label)
-#         }
-# })
-# ahrf_county
+ahrf_county_layout %>%
+        select(field, var_label) %>%
+        deframe() %>%
+        as.list() -> var_label(ahrf_county)
+var_label(ahrf_county)
 
 # Save it! ----------------------------------------------------------------
-devtools::use_data(ahrf_county_layout, overwrite=TRUE)
-devtools::use_data(ahrf_county, overwrite=TRUE)
+
+devtools::use_data(ahrf_county_layout, overwrite = TRUE)
+devtools::use_data(ahrf_county, overwrite = TRUE)
+
+# Delete raw data as it’s too large ---------------------------------------
+
+unlink(raw_src)
